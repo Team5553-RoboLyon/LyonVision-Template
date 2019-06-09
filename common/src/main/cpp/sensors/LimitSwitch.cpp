b2dd571839260ab7b3a4bed2bed96ab3caebbb68
Nathan Mutin <32647745+nathanmutin@users.noreply.github.com>
2019-06-09 12:27:14 +0200

    Suppression des fichiers "inutiles" - Templates pour Issues et Pull Request - Azure - Plugins TinkerBoard - Common

--- common/src/main/cpp/sensors/LimitSwitch.cpp
+++
@@ -1,9 +0,0 @@
-#include "sensors/LimitSwitch.h"
-
-curtinfrc::sensors::LimitSwitch::LimitSwitch(int channel) {
-  _switch = new frc::DigitalInput(channel);
-}
-
-bool curtinfrc::sensors::LimitSwitch::Get() {
-  return _switch->Get();
-}
