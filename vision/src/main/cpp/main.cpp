b4510ed8774acfc614d2763c634fe15fd28a7591
CJBuchel <connorbuchel@gmail.com>
2019-06-12 18:14:56 +0800

    Added #ifdef for Desktop/ Developer board

--- vision/src/main/cpp/main.cpp
+++ vision/src/main/cpp/main.cpp
@@ -4,10 +4,17 @@
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
+
+  #ifdef __DESKTOP__
+  std::cout << "Running on Desktop - imshow enabled" << std::endl;
+#else
+  std::cout << "Running embedded - imshow disabled" << std::endl;
+#endif
+
   std::cout << "Starting Vision Program" << std::endl;
   curtin_frc_vision vision;
   vision.run();
-  std::cout << "Vision Program Exited. Was that meant to happen? i doubt it lol." << std::endl;
+  std::cout << "Vision Program Exited. Was that meant to happen?" << std::endl;
   return -1;
 }
 #endif
--- vision/src/main/cpp/vision.cpp
+++ vision/src/main/cpp/vision.cpp
@@ -20,7 +20,7 @@ Rect bounding_rect;
 int thresh = 100;
 float height_offset;
 float width_offset;
-int ResWidth = 320, ResHeight = 240;
+int ResWidth = 640, ResHeight = 480;
 float width_goal;
 float height_goal;
 
@@ -49,117 +49,112 @@ void curtin_frc_vision::run() {
   cs::CvSource output = frc::CameraServer::GetInstance()->PutVideo("USB Camera", video_mode.width, video_mode.height);
 
   cv::Mat imgTracking{video_mode.height, video_mode.width, CV_8UC3};
-  
-
 
   while (true)
 	{
-		if (sink.GrabFrame(imgTracking) != 0) //if not success, wait and output
+		if (sink.GrabFrame(imgTracking) != 0)
 		{
-			cout << "Cannot read a frame from video stream" << endl;
-		}
+			// Convert input image to HSV
+			cvtColor(imgTracking, imgTracking, COLOR_BGR2HSV); //Convert the captugreen frame from BGR to HSV
+
+			
+			// Threshold the HSV image, keep only the green pixels
+			cv::inRange(imgTracking, cv::Scalar(35, 100, 100), cv::Scalar(78, 255, 255), imgTracking);
+			
+			erode(imgTracking, imgTracking, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+			dilate(imgTracking, imgTracking, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
+
+			// Detect edges using Canny
+			Canny(imgTracking, imgTracking, thresh, thresh * 2);
+
+			/// Find contours
+			vector<vector<Point> > contours;
+			vector<Vec4i> hierarchy;
+			threshold( imgTracking, imgTracking, thresh, 255, THRESH_BINARY );
+			findContours(imgTracking, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
 
-		// Convert input image to HSV
-		cvtColor(imgTracking, imgTracking, COLOR_BGR2HSV); //Convert the captugreen frame from BGR to HSV
-
-		
-		// Threshold the HSV image, keep only the green pixels
-		cv::inRange(imgTracking, cv::Scalar(35, 100, 100), cv::Scalar(78, 255, 255), imgTracking);
-	  
-		erode(imgTracking, imgTracking, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-		dilate(imgTracking, imgTracking, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)));
-
-		// Detect edges using Canny
-		Canny(imgTracking, imgTracking, thresh, thresh * 2);
-
-		/// Find contours
-		vector<vector<Point> > contours;
-		vector<Vec4i> hierarchy;
-		threshold( imgTracking, imgTracking, thresh, 255, THRESH_BINARY );
-		findContours(imgTracking, contours, hierarchy, RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
-
-
-		/// Find the convex hull object for each contour
-		vector<vector<Point> >hull(contours.size());
-		for (size_t i = 0; i < contours.size(); i++)
-		{
-			convexHull(contours[i], hull[i]);
-		}
-
-		/// Draw contours + hull results
-		imgTracking = cv::Mat::zeros(imgTracking.size(), CV_8UC3);
-		vector<Rect> boundRect( contours.size() );
-
-
-		for (size_t i = 0; i < contours.size(); i++)
-		{
-			Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-			drawContours(imgTracking, contours, (int)i, color);
-			drawContours(imgTracking, hull, (int)i, color);
-		}
-
-		/// Detect edges using Threshold
-		threshold( imgTracking, imgTracking, thresh, 255, THRESH_BINARY );
-		/// Find contoursBox
-
-
-		/// Approximate contoursBox to polygons + get bounding rects and circles
-		vector<vector<Point> > hull_poly( hull.size() );
-		vector<Point2f>center( hull.size() );
-		vector<float>radius( hull.size() );
-
-		for( int i = 0; i < hull.size(); i++ )
-			{ approxPolyDP( Mat(hull[i]), hull_poly[i], 3, true );
-			boundRect[i] = boundingRect( Mat(hull_poly[i]) );
-			minEnclosingCircle( (Mat)hull_poly[i], center[i], radius[i] );
+			/// Find the convex hull object for each contour
+			vector<vector<Point> >hull(contours.size());
+			for (size_t i = 0; i < contours.size(); i++)
+			{
+				convexHull(contours[i], hull[i]);
 			}
 
+			/// Draw contours + hull results
+			imgTracking = cv::Mat::zeros(imgTracking.size(), CV_8UC3);
+			vector<Rect> boundRect( contours.size() );
 
-		/// Draw polygonal contour + bonding rects + circles
-		for( int i = 0; i< hull.size(); i++ )
-		{
-			Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-			drawContours( imgTracking, hull_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-			bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
-			rectangle( imgTracking, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-			circle( imgTracking, center[i], (int)radius[i], color, 2, 8, 0 );
-		}
-			
 
-    // get the moments 
-	 	vector<Moments> mu(hull.size());
-    for( int i = 0; i<hull.size(); i++ )
-    { mu[i] = moments( hull[i], false ); }
- 
-    // get the centroid of figures.
-    vector<Point2f> mc(hull.size());
-    for( int i = 0; i<hull.size(); i++)
-    { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+			for (size_t i = 0; i < contours.size(); i++)
+			{
+				Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+				drawContours(imgTracking, contours, (int)i, color);
+				drawContours(imgTracking, hull, (int)i, color);
+			}
 
-		vector<double> weights;
-    for( int i = 0; i<hull.size(); i++ )
-    {
-      Scalar color = Scalar(167,151,0); // B G R values
-      //drawContours(imgTracking, hull_poly, i, color, 2, 8, hierarchy, 0, Point());
-	  	circle( imgTracking, mc[i], 4, color, -1, 8, 0 );
+			/// Detect edges using Threshold
+			threshold( imgTracking, imgTracking, thresh, 255, THRESH_BINARY );
+			/// Find contoursBox
 
-			// offsets from center
-			Point center = Point((mc[i].x), (mc[i].y));
-			width_offset = width_goal - center.x;
-			height_offset = height_goal - center.y;
+
+			/// Approximate contoursBox to polygons + get bounding rects and circles
+			vector<vector<Point> > hull_poly( hull.size() );
+			vector<Point2f>center( hull.size() );
+			vector<float>radius( hull.size() );
+
+			for( int i = 0; i < hull.size(); i++ )
+				{ approxPolyDP( Mat(hull[i]), hull_poly[i], 3, true );
+				boundRect[i] = boundingRect( Mat(hull_poly[i]) );
+				minEnclosingCircle( (Mat)hull_poly[i], center[i], radius[i] );
+				}
+
+
+			/// Draw polygonal contour + bonding rects + circles
+			for( int i = 0; i< hull.size(); i++ )
+			{
+				Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+				drawContours( imgTracking, hull_poly, i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+				bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
+				rectangle( imgTracking, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+				circle( imgTracking, center[i], (int)radius[i], color, 2, 8, 0 );
+			}
+				
+
+			// get the moments 
+			vector<Moments> mu(hull.size());
+			for( int i = 0; i<hull.size(); i++ )
+			{ mu[i] = moments( hull[i], false ); }
 	
-			stringstream temp;
-			temp << weights[i];
-			putText(imgTracking, temp.str(),Point(center.x,center.y+50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255));
-		}
+			// get the centroid of figures.
+			vector<Point2f> mc(hull.size());
+			for( int i = 0; i<hull.size(); i++)
+			{ mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
 
+			vector<double> weights;
+			for( int i = 0; i<hull.size(); i++ )
+			{
+				Scalar color = Scalar(167,151,0); // B G R values
+				//drawContours(imgTracking, hull_poly, i, color, 2, 8, hierarchy, 0, Point());
+				circle( imgTracking, mc[i], 4, color, -1, 8, 0 );
 
-		/// Show in a window
-		imshow("Output Image", imgTracking); // Shows the main tracking
-		output.PutFrame(imgTracking);
+				// offsets from center
+				Point center = Point((mc[i].x), (mc[i].y));
+				width_offset = width_goal - center.x;
+				height_offset = height_goal - center.y;
 		
-		waitKey(30);
-	
+				stringstream temp;
+				temp << weights[i];
+				putText(imgTracking, temp.str(),Point(center.x,center.y+50), FONT_HERSHEY_SIMPLEX, 1, Scalar(0,0,255));
+			}
+
+		#ifdef __DESKTOP__
+    	cv::imshow("Output Image", imgTracking); // Tinkerboards and Pi's don't like imshow, so we get rid of it
+		#endif
+    	output.PutFrame(imgTracking);
+			
+			waitKey(30);
+		}
+		std::cout << "Cannot read a frame from video stream. Is the external WebCam Plugged in?" << endl;
 	}
 }
