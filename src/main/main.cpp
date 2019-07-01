ac77c874fd419c65000406c9b4a5b8723365e6be
Nathan Mutin <32647745+nathanmutin@users.noreply.github.com>
2019-07-01 17:08:05 +0200

    Format

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,15 +4,15 @@
 
 #ifndef RUNNING_FRC_TESTS
 int main() {
-
-  #ifdef __DESKTOP__
+#ifdef __DESKTOP__
   std::cout << "Running on Desktop - imshow enabled" << std::endl;
 #else
   std::cout << "Running embedded - imshow disabled" << std::endl;
 #endif
 
   std::cout << "Starting Vision Program" << std::endl;
-  std::cout << "This Program was made for the 2019 Game DeepSpace, And tracks and outputs green values" << std::endl;
+  std::cout << "This Program was made for the 2019 Game DeepSpace, And tracks and outputs green values"
+            << std::endl;
   curtin_frc_vision vision;
   vision.run();
   std::cout << "Vision Program Exited. Was that meant to happen?" << std::endl;
